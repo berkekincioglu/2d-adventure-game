8d243a019d9382bdd2780474d43cd49048bd3e4e
berkekincioglu <berkekinci2000@hotmail.com>
2026-03-04 22:33:57 +0300

    pyhsics rigidbody, colliders

--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    private Rigidbody2D rb;
+    private Vector2 move;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,14 +13,20 @@ public class PlayerController : MonoBehaviour
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
         MoveAction.Enable();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
     }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rb.position + 3.0f * Time.fixedDeltaTime * move;
+        rb.MovePosition(position);
+    }
+
 }
