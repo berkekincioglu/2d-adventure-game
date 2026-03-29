9356d797348dfe4169ca6ef5c41d7a940ccc3473
berkekincioglu <berkekinci2000@hotmail.com>
2026-03-29 22:48:06 +0300

    extras

--- Assets/_2DAdventureGame/Scripts/GameManager.cs
+++ Assets/_2DAdventureGame/Scripts/GameManager.cs
@@ -7,11 +7,22 @@ public class GameManager : MonoBehaviour
     [SerializeField] private PlayerController player;
     EnemyController[] enemies;
     public UIHandler uiHandler;
+    private int enemiesFixed = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         enemies = FindObjectsByType<EnemyController>(FindObjectsSortMode.None);
+
+        foreach (var enemy in enemies)
+        {
+            enemy.OnFixed += HandleEnemyFixed;
+        }
+        uiHandler.SetCounter(0, enemies.Length);
+        player.OnTalkedToNPC += HandlePlayerTalkedToNPC;
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -21,11 +32,11 @@ public class GameManager : MonoBehaviour
             uiHandler.DisplayLoseScreen();
             Invoke(nameof(ReloadScene), 3f);
         }
-        if (AllEnemiesFixed())
-        {
-            uiHandler.DisplayWinScreen();
-            Invoke(nameof(ReloadScene), 3f);
-        }
+        // if (AllEnemiesFixed())
+        // {
+        //     uiHandler.DisplayWinScreen();
+        //     Invoke(nameof(ReloadScene), 3f);
+        // }
     }
 
     void ReloadScene()
@@ -43,5 +54,35 @@ public class GameManager : MonoBehaviour
             }
         }
         return true;
+    }
+
+    void OnDestroy()
+    {
+        foreach (var enemy in enemies)
+        {
+            if (enemy != null)
+                enemy.OnFixed -= HandleEnemyFixed;
+        }
+
+        player.OnTalkedToNPC -= HandlePlayerTalkedToNPC;
+    }
+
+    void HandleEnemyFixed()
+    {
+        enemiesFixed++;
+        uiHandler.SetCounter(enemiesFixed, enemies.Length);
+    }
+
+    void HandlePlayerTalkedToNPC()
+    {
+        if (AllEnemiesFixed())
+        {
+            uiHandler.DisplayWinScreen();
+            Invoke(nameof(ReloadScene), 3f);
+        }
+        else
+        {
+            UIHandler.instance.DisplayDialogue();
+        }
     }
 }
